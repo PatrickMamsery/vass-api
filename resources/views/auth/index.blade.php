66fa5b40bb92f517426deae970c52f29274275c3
PatrickMamsery <pkrobert1612@gmail.com>
2024-04-19 08:21:38 +0300

    chore: change auth login page

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -3,7 +3,8 @@
     <div class="auth-wrapper background">
         <div class="auth-form">
             <div class="header">
-                <img src="/images/logo_white_red.png" alt="" style="width: 120px">
+                {{-- <img src="/images/logo_white_red.png" alt="" style="width: 120px"> --}}
+                <h1 class="title">VASS Admin Dashboard</h1>
             </div>
             <div class="body">
                 <livewire:authswitch>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,22 @@
  <div class="login">
      <div class="title">Sign In</div>
      <div class="error">
-        <x-auth-session-status  :status="session('status')" />
-        <x-auth-validation-errors  :errors="$errors" />
+         <x-auth-session-status :status="session('status')" />
+         <x-auth-validation-errors :errors="$errors" />
      </div>
      <form method="POST" action="{{ route('login') }}">
-        @csrf
+         @csrf
          <div class="form-group">
-             <input type="email" id="email" class="form-control"  placeholder="Enter email"   name="email" :value="old('email')" required autofocus>
+             <input type="email" id="email" class="form-control" placeholder="Enter email" name="email"
+                 :value="old('email')" required autofocus>
          </div>
          <div class="form-group">
-             <input id="password" type="password" class="form-control" placeholder="Enter password" type="password" name="password" required autocomplete="current-password">
+             <input id="password" type="password" class="form-control" placeholder="Enter password" type="password"
+                 name="password" required autocomplete="current-password">
          </div>
          <button type="submit" class="btn btn-custom mt-4">{{ __('Log in') }}</button>
          <div class="text-center mt-2">
-             <a wire:click="switch" >Forgot password ?</a>
+             <a wire:click="switch">Forgot password?</a>
          </div>
      </form>
  </div>
